e2803953847b755f30ab57a7e4d63d944ce32f4b
morphley <developer.omari@gmail.com>
2018-03-27 22:45:55 +0200

    Commit4:  ImagePicker and save image into Storage and info into DB
    User Profile Picture
    Instagram Firebase
    
    Now that the necessary Firebase SDKs have been installed and the user
    authentication step is complete, let's dive into the code that allows
    more data persistence.  First we'll implement the feature that saves a
    username into Firebase Database.  Then we move onto using
    UIImagePickerController to allow the user selection of the profile
    image in our application.
    
    Lastly, we upload the image into Firebase Storage, which conveniently
    gives us back a URL for retrieval later.  Finally we take this URL and
    save it onto our user along with their username.

--- ViewController.swift
+++ ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 import Firebase
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     //closure-block
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
-        // button.backgroundColor = .red
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
         button.clipsToBounds = true
+        button.addTarget(self, action: #selector(handlePlusPhoto) , for: .touchUpInside)
         return button
     }() // execute closure block
+    
+    
+    @objc func handlePlusPhoto(){
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+             plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+            
+        }else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            
+             plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+       
+        
+    
+        
+        
+       dismiss(animated: true, completion:  nil)
+    }
     
     // problems using frame when rotating device layout is not longer true solution autolayout
     
@@ -91,9 +121,8 @@ class ViewController: UIViewController {
     
     
     @objc func handleSignUp() {
-
-
         
+        // take the information from the sign-up screen
         guard let email = emailTextField.text, email.characters.count > 0 else { return }
         guard let username = usernameTextField.text, username.characters.count > 0 else { return }
         guard let password = passwordTextField.text, password.characters.count > 0 else { return }
@@ -102,14 +131,57 @@ class ViewController: UIViewController {
         Auth.auth().createUser(withEmail: email, password: password) { (user: User?, error: Error?) in
             
             if let err = error {
-                
                 print("Failed to create User: " , err.localizedDescription )
+                return // if user is already signed up
             }
             
             print("Succesfully created User: " , user?.uid ?? "")
-        }
+            
+            // Try to upload Image to Storage
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else { return }
+            
+            let filename = NSUUID().uuidString
+            Storage.storage().reference().child("profile_images").child(filename).putData(uploadData, metadata: nil, completion: { (metadata, err) in
+                
+                if let err = err {
+                    print("Failed to upload profile image: ", err)
+                    return
+                }
+                
+                guard  let profileImageUrl = metadata?.downloadURL()?.absoluteString else { return }
+                print("Succesfully uploaded profile_image :", profileImageUrl)
+                
 
- 
+                guard let uid = user?.uid else { return }
+                
+                let dictionaryValues = ["username": username, "password": password, "profileImageUrl": profileImageUrl]
+                let values = [uid: dictionaryValues]
+                print(values)
+                
+                
+                Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+                    
+                    
+                    if let err = err {
+                        print("Failed to save user info into db: ", err)
+                        return
+                    }
+                    
+                    print("Succesfully stored user into db")
+                    
+                })
+                
+            })
+            
+            
+          
+            
+           
+            
+        }
+        
+        
     }
     
     
@@ -198,22 +270,6 @@ extension UIView{
 
 
 
-
-
-
-
-
-extension CGFloat {
-    static var random: CGFloat {
-        return CGFloat(arc4random()) / CGFloat(UInt32.max)
-    }
-}
-
-extension UIColor {
-    static var random: UIColor {
-        return UIColor(red: .random, green: .random, blue: .random, alpha: 1.0)
-    }
-}
 
 
 
